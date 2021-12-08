9d7bdd1a812ed3cd08f27de3ca7b22ee638d4f1c
Dom3005 <30704218+Dom3005@users.noreply.github.com>
2021-12-08 11:27:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ no3 = ["hat ins Bett geschissen", "ist vor mir verreckt", "will nicht aufhören 
     "hat mich zum Islam konvertiert, habibi", "wollte mich von Verschwörungstheorien überzeugen", "hat meine lebensgroßen Pappaufsteller von Herr Zimmermann gefunden",
     "hat sich eingeschissen", "hat spontan einen Genozid begangen", "hat den dritten Weltkrieg gestartet", "hat mich mit Corona angesteckt", "hat mich abgezogen", "",
     "hat mich gescammt", "hat mich mit einem Raketenwerfer abgeschossen", "hat einen Terroranschlag verübt", "hat eine Gedichtsanalyse von mir verlangt",
-    "wollte die UdSSR wiederbeleben"];
+    "wollte die UdSSR wiederbeleben", "hat mich gewaterboarded"];
 
 classNames = ["no1", "no2", "no3"];
 fields = [no1, no2, no3];
