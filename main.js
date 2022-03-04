dd36f673e715c65568608ce99c4d162d739cab00
Dom3005 <30704218+Dom3005@users.noreply.github.com>
2022-03-04 11:02:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ no3 = ["hat ins Bett geschissen", "ist vor mir verreckt", "will nicht aufhören 
     "hat einen Molotov auf mich geworfen", "hat mich nicht vorbei gelassen", "hat Top 3 auf der FBI most-wanted list erreicht", "ist für den Kollaps der UdSSR verantwortlich",
     "hat {state} völkerrechtswidrig als einen souveränen Staat anerkannt", "hat {state} {country} zugesprochen", "hat {country} annektiert", "ist in Polen eingefallen",
     "hat {country} seinem Reich angeschlossen", "hat sich {country} angeschlossen", "hat einen Putsch in {country} gestartet", "hat den Nationalen Notstand ausgerufen",
-    "hat {country} den Krieg erklärt", "hat den eigenständigen Staat von {country} gegründet", "ist eine Massenvernichtungswaffe",
+    "hat {country} den Krieg erklärt", "hat den eigenständigen Staat von {country} gegründet", "ist eine Massenvernichtungswaffe", "ist nun Bürgermeister von Phuket (Fuck-it)",
     "hat mich unfreiwillig zu Berufs- & Studienberatung gezwungen", "konnte mir kein Öl anektieren", "konnte mir keine Ferndiagnose aufstellen"];
 
 states = ["Bayern", "NRW", "Berlin", "Hamburg", "Sachsen", "Florida", "Texas", "Taiwan", "Ostdeutschland", "Kentucky", "den Islamischen Staat", "IKEA", "die gesamte Erde",
